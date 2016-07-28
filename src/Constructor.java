34b389e835b47ad21ed4af6e0d5c889ef4fe597b
Dolly Aswin <dolly.aswin@gmail.com>
2016-07-28 13:48:33 +0700

    - Add Machine Class - Add example for constructor usage

---
+++ src/Constructor.java
@@ -0,0 +1,9 @@
+import com.aqilix.learning.java.Machine;
+
+public class Constructor {
+	public static void main(String[] args) {
+		Machine machine1 = new Machine();
+		Machine machine2 = new Machine("Sewing");
+		Machine machine3 = new Machine("Sewing", "Electric");
+	}
+}
---
+++ src/com/aqilix/learning/java/Machine.java
@@ -0,0 +1,45 @@
+package com.aqilix.learning.java;
+
+public class Machine {
+	private String name;
+	
+	private String type;
+	
+	public Machine() {
+		System.out.println("First constructor running!");
+//		this("Sewing");
+	}
+	
+	public Machine(String name) {
+		System.out.println("Second constructor running!");
+//		this(name, "electric");
+	}
+	
+	public Machine(String name, String type) {
+		System.out.println("Third constructor running!");
+		this.setName(name);
+		this.setType(type);
+		this.displayInfo();
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void displayInfo() {
+		System.out.println("Name: " + this.getName());
+		System.out.println("Type: " + this.getType());
+	}
+}
