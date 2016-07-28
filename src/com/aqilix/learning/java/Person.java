dd20ad26f3d51167208a8fac3cea935ef80d52e7
Dolly Aswin <dolly.aswin@gmail.com>
2016-07-28 13:25:13 +0700

    Add Person Class

---
+++ src/com/aqilix/learning/java/Person.java
@@ -0,0 +1,32 @@
+package com.aqilix.learning.java;
+
+public class Person {
+	String name;
+	
+	int age;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void move(String direction, double distance) {
+		System.out.println("Move to " + direction + " with distance " + distance);
+	}
+	
+	public void displayInfo() {
+		System.out.println("Name: " + this.getName());
+		System.out.println("Age: " + this.getAge());
+	}
+}
