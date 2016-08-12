8923937841fd27d599f6f4af88d4f8554e1d35bc
Dolly Aswin <dolly.aswin@gmail.com>
2016-08-12 08:50:36 +0700

    Remove obsolete java code

--- src/Generics.java
+++ src/Generics.java
@@ -5,15 +5,6 @@ import com.aqilix.learning.java.Machine;
 public class Generics {
 
 	public static void main(String[] args) {
-		// before java 5
-		ArrayList list = new ArrayList();
-		list.add("apple");
-		list.add("banana");
-		list.add("orange");
-		
-		String fruit = (String) list.get(1);
-		System.out.println(fruit);
-		
 		// modern style
 	    ArrayList<String> strings = new ArrayList<String>();
 	    strings.add("cat");
